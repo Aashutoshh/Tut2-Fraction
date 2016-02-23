6bd7ad83c2d75772732ab76a860c6282cabe61b3
Aashutoshh <214506607@stu.ukzn.ac.za>
2016-02-23 15:11:19 +0200

    Add operations

--- Fraction.cpp
+++ Fraction.cpp
@@ -1,14 +1,45 @@
 #include <iostream>
 class Fraction{
+private:
+	int denom;
+	int numo;
 
 public: 
-	Fraction() {
-
+	Fraction(int num , int den) {
+		denom = den;
+		numo = num 
 	}
 
 
-	~Fraction()
-	{
+	~Fraction(){
 
 	}
+
+	void getNumDenom(int num, int den){
+		numo = num;
+		denom = den;
+	}
+
+	void setNumDenom( int *num, int *den){
+		*num = numo;
+		*den = denom;
+	}
+
+	double add(int num, int den){
+		return num / den + numo / denom;
+
+	}
+
+	double subtract(int num, int den){
+		return add(num, -den);
+	}
+
+	double multiply(int num, int den){
+		return num / den * numo / denom;
+	}
+
+	double divide(int num , int den){
+		return multiply(den,num);
+	}
+
 };
